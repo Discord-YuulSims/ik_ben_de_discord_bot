7cee4c42ca9c8032e3aed30634b03e9c851251d1
Discord-YuulSims <Discord.YuulSims@noreply.github.com>
2020-09-12 12:44:36 +0200

    Update links.js

--- beackup/commands/links.js
+++ beackup/commands/links.js
@@ -4,8 +4,8 @@ module.exports.run = async (client, message, args) => {
 
     var linkjesEmbed = new discord.MessageEmbed()
     .setTitle("Linkjes van YuulSims.")
-    .setDescription(`[Klik hier]`, (www.youtube.com))
-    .addField('test', "https://i.stack.imgur.com/Ke92A.png")
+    .setDescription(`[Klik hier]`, '(www.youtube.com)')
+    .addField('test', 'https://i.stack.imgur.com/Ke92A.png')
 
     message.channel.send(linkjesEmbed)
 }
