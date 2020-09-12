ea52f676c297471d53f2a199278acf4210740fdb
Discord-YuulSims <Discord.YuulSims@noreply.github.com>
2020-09-12 12:18:16 +0200

    Update leden.js

--- beackup/commands/leden.js
+++ beackup/commands/leden.js
@@ -2,7 +2,25 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-  
+    var ledenTotal = message.guild.memberCount;
+
+    var Bots = message.guild.members.cache.filter(m => m.user.bot).size;
+
+    var mernsen = ledenTotal - Bots;
+
+    var online = message.guild.members.cache.filter(m => m.user.presence.status == "online" || m.user.presence.status == "dnd" || m.user.presence.status == "idle");
+
+    var ledenEmbed = new discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setAuthor(`${message.guild.name}`, `${message.guild.avatarURL({ size: 4096 })}`)
+        .addField("Leden:", ledenTotal, true)
+        .addField("Mensen:", mernsen)
+        .addField("Bots:", Bots, true)
+        .addField("Online:", online, true);
+
+    message.channel.send(ledenEmbed);
+
+
 }
 
 
@@ -10,6 +28,6 @@ module.exports.run = async (client, message, args) => {
 module.exports.help = {
 
     name: "leden",
-    
+
 
 }
